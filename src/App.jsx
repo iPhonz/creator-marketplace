9a1ae598a37037d936d250b7b994e6669541a729
Phonz <119698504+iPhonz@users.noreply.github.com>
2025-02-17 00:39:22 -0800

    Add App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CreatorMarketplace from './pages/CreatorMarketplace';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <CreatorMarketplace />
+    </div>
+  );
+}
+
+export default App;
